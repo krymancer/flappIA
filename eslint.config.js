d8cb5c4f27afb01f7d176af92d9b903ea832611d
krymancer <junior.nascm@gmail.com>
2026-07-13 22:12:01 +0000

    fix: ignore dist in eslint config and wrap globals line

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,19 @@
 import js from '@eslint/js';
 
 export default [
+  { ignores: ['dist/**'] },
   js.configs.recommended,
   {
     files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 2023,
       sourceType: 'module',
-      globals: { window: 'readonly', document: 'readonly', Image: 'readonly', requestAnimationFrame: 'readonly' },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        Image: 'readonly',
+        requestAnimationFrame: 'readonly',
+      },
     },
     rules: {
       'no-unused-vars': 'warn',
